18bb0384b9d936ee4f55a844f76423518e64174e
sanyaleks2012-lab <sany.aleks2012@gmail.com>
2026-05-05 02:00:53 +0300

    Update from parent directory

--- 2/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Running Git Push Script...
-
-:: Запуск PowerShell скрипта с обходом ограничений
-powershell -NoProfile -ExecutionPolicy Bypass -File "1.ps1"
-
-echo.
-echo Done! Press any key to close.
-pause
